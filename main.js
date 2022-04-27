339c409ad325a1cd521a305c8cc742768d10fc44
David <Davidfriedchicken@gmail.com>
2022-04-27 11:23:44 -0700

    Added some html, and made an onclick function

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+$('.aboutme').on('click', function() {
+    $('.about-me-wrapper').css('display', 'block')
+    $('.about-me-wrapper').css('height', '1000px')
+    $('.about-me-wrapper').css('top', '10%')
+    $('.aboutme').css('top', '5%')
+    $('.projects').css('display', 'none')
+})
