a0b812be84b6062a7d8e28fa0e8ff2046b513cfc
David <Davidfriedchicken@gmail.com>
2022-04-28 13:22:30 -0700

    Made the snow animation repeat itself

--- main.js
+++ main.js
@@ -150,7 +150,13 @@ let x20 = snowball20.position() */
 
 let length = window.innerWidth;
 
+let height = window.innerHeight
+
+console.log(height)
+
 let randomNum1 = Math.floor(Math.random() * length)
+
+/*
 
 $('#snowball1').css('transform', 'translateX(' + randomNum1 + 'px)')
 
@@ -162,7 +168,7 @@ $('#snowball1').css('transition-timing-function', 'linear')
 
 $('#snowball1').css('transform', 'translate(' + x1.left + 'px, 2000px)')
 
-console.log(x1.left)
+console.log(x1.left) */
 
 
 /////////////TWO///////////////////
@@ -185,7 +191,7 @@ function funcsnowball2() {
     
     $('#snowball2').css('transition-timing-function', 'linear')
     
-    $('#snowball2').css('transform', 'translate(' + x2.left + 'px, 2000px)')
+    $('#snowball2').css('transform', 'translate(' + x2.left + 'px,  1300px)')
     
     console.log(x2.left)
     
@@ -204,7 +210,9 @@ function funcsnowball3() {
 
     let randomNum3 = Math.floor(Math.random() * length)
 
-    $('#snowball2').css('transition', '0s')
+    $('#snowball3').css('transition', '0s')
+
+    $('#snowball3').css('transform', 'translateY(-50px)')
     
     $('#snowball3').css('transform', 'translateX(' + randomNum3 + 'px)')
 
@@ -214,13 +222,15 @@ function funcsnowball3() {
     
     $('#snowball3').css('transition-timing-function', 'linear')
     
-    $('#snowball3').css('transform', 'translate(' + x3.left + 'px, 2000px)')
+    $('#snowball3').css('transform', 'translate(' + x3.left + 'px, 1300px)')
     
-    console.log(x3.left)
+    console.log(height)
     
     }
     
-    setTimeout(funcsnowball3, 1000)
+    setTimeout(funcsnowball3, 2000)
+
+    setInterval(funcsnowball3, 12000)
 
 
 
@@ -232,7 +242,9 @@ function funcsnowball3() {
 
         let randomNum4 = Math.floor(Math.random() * length)
 
-        $('#snowball2').css('transition', '0s')
+        $('#snowball4').css('transition', '0s')
+
+        $('#snowball4').css('transform', 'translateY(-50px)')
         
         $('#snowball4').css('transform', 'translateX(' + randomNum4 + 'px)')
         
@@ -242,13 +254,15 @@ function funcsnowball3() {
         
         $('#snowball4').css('transition-timing-function', 'linear')
         
-        $('#snowball4').css('transform', 'translate(' + x4.left + 'px, 2000px)')
+        $('#snowball4').css('transform', 'translate(' + x4.left + 'px, 1300px)')
         
         console.log(x4.left)
         
         }
         
-        setTimeout(funcsnowball4, 1000)
+        setTimeout(funcsnowball4, 3000)
+
+        setInterval(funcsnowball4, 13000)
 
 
 //555555555555555555555555555555555555555555555555555
@@ -258,6 +272,10 @@ function funcsnowball3() {
 function funcsnowball5() {
 
     let randomNum5 = Math.floor(Math.random() * length)
+
+    $('#snowball5').css('transition', '0s')
+
+    $('#snowball5').css('transform', 'translateY(-50px)')
     
     $('#snowball5').css('transform', 'translateX(' + randomNum5 + 'px)')
     
@@ -267,13 +285,15 @@ function funcsnowball5() {
     
     $('#snowball5').css('transition-timing-function', 'linear')
     
-    $('#snowball5').css('transform', 'translate(' + x5.left + 'px, 2000px)')
+    $('#snowball5').css('transform', 'translate(' + x5.left + 'px, 1300px)')
     
     console.log(x5.left)
     
     }
     
-    setTimeout(funcsnowball5, 1000)
+    setTimeout(funcsnowball5, 4000)
+
+    setInterval(funcsnowball5, 14000)
 
 
     //666666666666666666666666666666666666
@@ -281,6 +301,10 @@ function funcsnowball5() {
     function funcsnowball6() {
 
         let randomNum6 = Math.floor(Math.random() * length)
+
+        $('#snowball6').css('transition', '0s')
+
+        $('#snowball6').css('transform', 'translateY(-50px)')
         
         $('#snowball6').css('transform', 'translateX(' + randomNum6 + 'px)')
     
@@ -290,13 +314,15 @@ function funcsnowball5() {
         
         $('#snowball6').css('transition-timing-function', 'linear')
         
-        $('#snowball6').css('transform', 'translate(' + x6.left + 'px, 2000px)')
+        $('#snowball6').css('transform', 'translate(' + x6.left + 'px, 1300px)')
         
-        console.log(x6.left)
+        console.log(height)
         
         }
         
-        setTimeout(funcsnowball6, 1000)
+        setTimeout(funcsnowball6, 5000)
+
+        setInterval(funcsnowball6, 15000)
 
 
 
@@ -306,6 +332,10 @@ function funcsnowball5() {
         function funcsnowball7() {
 
             let randomNum7 = Math.floor(Math.random() * length)
+
+            $('#snowball7').css('transition', '0s')
+
+            $('#snowball7').css('transform', 'translateY(-50px)')
             
             $('#snowball7').css('transform', 'translateX(' + randomNum7 + 'px)')
         
@@ -315,13 +345,15 @@ function funcsnowball5() {
             
             $('#snowball7').css('transition-timing-function', 'linear')
             
-            $('#snowball7').css('transform', 'translate(' + x7.left + 'px, 2000px)')
+            $('#snowball7').css('transform', 'translate(' + x7.left + 'px, 1300px)')
             
             console.log(x7.left)
             
             }
             
-            setTimeout(funcsnowball7, 1000)
+            setTimeout(funcsnowball7, 7000)
+
+            setInterval(funcsnowball7, 17000)
 
 
             //8888888888888888888888888
@@ -330,6 +362,10 @@ function funcsnowball5() {
             function funcsnowball8() {
 
                 let randomNum8 = Math.floor(Math.random() * length)
+
+                $('#snowball8').css('transition', '0s')
+
+                $('#snowball8').css('transform', 'translateY(-50px)')
                 
                 $('#snowball8').css('transform', 'translateX(' + randomNum8 + 'px)')
             
@@ -339,16 +375,218 @@ function funcsnowball5() {
                 
                 $('#snowball8').css('transition-timing-function', 'linear')
                 
-                $('#snowball8').css('transform', 'translate(' + x8.left + 'px, 2000px)')
+                $('#snowball8').css('transform', 'translate(' + x8.left + 'px, 1300px)')
                 
                 console.log(x8.left)
                 
                 }
                 
-                setTimeout(funcsnowball8, 1000)
+                setTimeout(funcsnowball8, 8000)
+
+                setInterval(funcsnowball8, 18000)
 
 
                 //99999999999999999999999
+
+
+                function funcsnowball9() {
+
+                    let randomNum9 = Math.floor(Math.random() * length)
+    
+                    $('#snowball9').css('transition', '0s')
+    
+                    $('#snowball9').css('transform', 'translateY(-50px)')
+                    
+                    $('#snowball9').css('transform', 'translateX(' + randomNum9 + 'px)')
+                
+                    let x9 = snowball9.position()
+                    
+                    $('#snowball9').css('transition', '10s')
+                    
+                    $('#snowball9').css('transition-timing-function', 'linear')
+                    
+                    $('#snowball9').css('transform', 'translate(' + x9.left + 'px, 1300px)')
+                    
+                    console.log(x9.left)
+                    
+                    }
+                    
+                    setTimeout(funcsnowball9, 9000)
+    
+                    setInterval(funcsnowball9, 19000)
+
+                    //1111111100000000000000
+
+
+                    function funcsnowball10() {
+
+                        let randomNum10 = Math.floor(Math.random() * length)
+        
+                        $('#snowball10').css('transition', '0s')
+        
+                        $('#snowball10').css('transform', 'translateY(-50px)')
+                        
+                        $('#snowball10').css('transform', 'translateX(' + randomNum10 + 'px)')
+                    
+                        let x10 = snowball10.position()
+                        
+                        $('#snowball10').css('transition', '10s')
+                        
+                        $('#snowball10').css('transition-timing-function', 'linear')
+                        
+                        $('#snowball10').css('transform', 'translate(' + x10.left + 'px, 1300px)')
+
+                    }
+
+                    setTimeout(funcsnowball10, 10000)
+    
+                    setInterval(funcsnowball10, 20000)
+                
+                        
+                    //111111111111-----1111111111
+
+
+                    function funcsnowball11() {
+
+                        let randomNum11 = Math.floor(Math.random() * length)
+        
+                        $('#snowball11').css('transition', '0s')
+        
+                        $('#snowball11').css('transform', 'translateY(-50px)')
+                        
+                        $('#snowball11').css('transform', 'translateX(' + randomNum11 + 'px)')
+                    
+                        let x11 = snowball11.position()
+                        
+                        $('#snowball11').css('transition', '10s')
+                        
+                        $('#snowball11').css('transition-timing-function', 'linear')
+                        
+                        $('#snowball11').css('transform', 'translate(' + x11.left + 'px, 1300px)')
+                        
+                        console.log(x11.left)
+                        
+                        }
+                        
+                        setTimeout(funcsnowball11, 11000)
+        
+                        setInterval(funcsnowball11, 21000)
+
+
+                        //1111111111112222222222222222222222
+
+
+                        function funcsnowball12() {
+
+                            let randomNum12 = Math.floor(Math.random() * length)
+            
+                            $('#snowball12').css('transition', '0s')
+            
+                            $('#snowball12').css('transform', 'translateY(-50px)')
+                            
+                            $('#snowball12').css('transform', 'translateX(' + randomNum12 + 'px)')
+                        
+                            let x12 = snowball12.position()
+                            
+                            $('#snowball12').css('transition', '10s')
+                            
+                            $('#snowball12').css('transition-timing-function', 'linear')
+                            
+                            $('#snowball12').css('transform', 'translate(' + x12.left + 'px, 1300px)')
+
+                        }
+
+                        setTimeout(funcsnowball12, 12000)
+    
+                        setInterval(funcsnowball12, 22000)
+                        
+                            
+
+                        //111111111111333333333333333333333333
+
+
+                        function funcsnowball13() {
+
+                            let randomNum13 = Math.floor(Math.random() * length)
+            
+                            $('#snowball13').css('transition', '0s')
+            
+                            $('#snowball13').css('transform', 'translateY(-50px)')
+                            
+                            $('#snowball13').css('transform', 'translateX(' + randomNum13 + 'px)')
+                        
+                            let x13 = snowball13.position()
+                            
+                            $('#snowball13').css('transition', '10s')
+                            
+                            $('#snowball13').css('transition-timing-function', 'linear')
+                            
+                            $('#snowball13').css('transform', 'translate(' + x13.left + 'px, 1300px)')
+
+                        }
+
+                        setTimeout(funcsnowball13, 13000)
+    
+                        setInterval(funcsnowball13, 23000)
+
+                        //1111111111114444444444444444444
+
+
+                        function funcsnowball14() {
+
+                            let randomNum14 = Math.floor(Math.random() * length)
+            
+                            $('#snowball14').css('transition', '0s')
+            
+                            $('#snowball14').css('transform', 'translateY(-50px)')
+                            
+                            $('#snowball14').css('transform', 'translateX(' + randomNum14 + 'px)')
+                        
+                            let x14 = snowball14.position()
+                            
+                            $('#snowball14').css('transition', '10s')
+                            
+                            $('#snowball14').css('transition-timing-function', 'linear')
+                            
+                            $('#snowball14').css('transform', 'translate(' + x14.left + 'px, 1300px)')
+                            
+                            console.log(x14.left)
+                            
+                            }
+                            
+                            setTimeout(funcsnowball14, 14000)
+            
+                            setInterval(funcsnowball14, 24000)
+
+                            
+                            //111111111111555555555555555555555
+
+
+                            function funcsnowball15() {
+
+                                let randomNum15 = Math.floor(Math.random() * length)
+                
+                                $('#snowball15').css('transition', '0s')
+                
+                                $('#snowball15').css('transform', 'translateY(-50px)')
+                                
+                                $('#snowball15').css('transform', 'translateX(' + randomNum15 + 'px)')
+                            
+                                let x15 = snowball15.position()
+                                
+                                $('#snowball15').css('transition', '10s')
+                                
+                                $('#snowball15').css('transition-timing-function', 'linear')
+                                
+                                $('#snowball15').css('transform', 'translate(' + x15.left + 'px, 1300px)')
+                                
+                                console.log(x15.left)
+                                
+                                }
+                                
+                                setTimeout(funcsnowball15, 15000)
+                
+                                setInterval(funcsnowball15, 25000)
 
 
 /*
